73237f33ea40ec5c94192abe42352827166e102f
MWTU-TECH-AI <mistarkelvin@outlook.com>
2024-07-19 19:10:12 +0300

    Update mwtute.cjs

--- mwtutech/mwtute.cjs
+++ mwtutech/mwtute.cjs
@@ -6,7 +6,7 @@ const Crypto = require("crypto")
 const ff = require('fluent-ffmpeg')
 const webp = require("node-webpmux")
 const path = require("path")
-/* MWTU-MD V2 */
+/*. MWTU-MD V2 */
 async function imageToWebp (media) {
 /* MWTU-MD V2 */
     const tmpFileOut = path.join(tmpdir(), `${Crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`)
